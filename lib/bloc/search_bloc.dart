aa32688075629c26a74f38f5411c51660f8aa871
lonewanderer27 <adrianejames27@gmail.com>
2025-04-07 17:44:35 +0800

    feat(search_bloc): implement on search event

--- lib/bloc/search_bloc.dart
+++ lib/bloc/search_bloc.dart
@@ -1,13 +1,22 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:planets/constants.dart';
 
 part 'search_event.dart';
 part 'search_state.dart';
 
 class SearchBloc extends Bloc<SearchEvent, SearchState> {
   SearchBloc() : super(SearchInitial()) {
-    on<SearchEvent>((event, emit) {
-      // TODO: implement event handler
+    on<SearchQuery>((SearchQuery event, Emitter<SearchState> emit) {
+      List<Planets> searchedPlanets = [];
+
+      for (var element in Planets.values) {
+        if (element.toString().toLowerCase().contains(event.query)) {
+          searchedPlanets.add(element);
+        }
+      }
+
+      emit(SearchLoaded(searchedPlanets));
     });
   }
 }
