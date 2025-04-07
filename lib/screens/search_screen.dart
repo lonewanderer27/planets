e7c9a11159eba4134da9d0db187db572ce0007a0
lonewanderer27 <adrianejames27@gmail.com>
2025-04-07 19:37:28 +0800

    feat(search_screen): add search bar

--- lib/screens/search_screen.dart
+++ lib/screens/search_screen.dart
@@ -1,10 +1,39 @@
 import 'package:flutter/material.dart';
 
-class SearchScreen extends StatelessWidget {
+class SearchScreen extends StatefulWidget {
   const SearchScreen({super.key});
 
   @override
+  State<SearchScreen> createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: TextField(
+          controller: _searchController,
+          style: const TextStyle(color: Colors.white),
+          decoration: InputDecoration(
+            hintText: 'Enter planet name',
+            hintStyle: const TextStyle(color: Colors.white70),
+            suffixIcon: _searchController.text.isNotEmpty
+                ? IconButton(
+                    icon: const Icon(Icons.clear),
+                    onPressed: () {
+                      _searchController.clear();
+                      setState(() {});
+                    },
+                  )
+                : const Icon(Icons.search),
+          ),
+          textInputAction: TextInputAction.search,
+        ),
+        elevation: 0,
+      ),
+    );
   }
 }
